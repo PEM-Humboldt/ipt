058d6a3dafa4fb3be8b1bd160f68c723b4be67cd
alegnaaived <adevia@humboldt.org.co>
2022-01-22 23:46:16 -0500

    Solve conflict and add CEIBA Logo

--- src/main/java/org/gbif/ipt/action/portal/ResourceFileAction.java
+++ src/main/java/org/gbif/ipt/action/portal/ResourceFileAction.java
@@ -38,7 +38,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * The Action responsible for serving datadir resource files.
